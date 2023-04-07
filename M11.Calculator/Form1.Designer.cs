80f59e84d70db4732e33e74ee26caa8d208e1671
Laura Wilson <ljw5533@psu.edu>
2023-04-07 18:58:48 -0400

    Add project files.

---
+++ M11.Calculator/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace M11.Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            zeroKey = new Button();
+            decPoint = new Button();
+            addOp = new Button();
+            oneKey = new Button();
+            twoKey = new Button();
+            threeKey = new Button();
+            subtractOp = new Button();
+            fourKey = new Button();
+            fiveKey = new Button();
+            equalOp = new Button();
+            inverseX = new Button();
+            squareRoot = new Button();
+            sixKey = new Button();
+            multOp = new Button();
+            sevenKey = new Button();
+            eightKey = new Button();
+            nineKey = new Button();
+            divideOp = new Button();
+            backKey = new Button();
+            clearAllKey = new Button();
+            posNeg = new Button();
+            clearLastKey = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // zeroKey
+            // 
+            zeroKey.Location = new Point(5, 407);
+            zeroKey.Margin = new Padding(3, 4, 3, 4);
+            zeroKey.Name = "zeroKey";
+            zeroKey.Size = new Size(145, 53);
+            zeroKey.TabIndex = 0;
+            zeroKey.Text = "0";
+            zeroKey.UseVisualStyleBackColor = true;
+            zeroKey.Click += zeroKey_Click;
+            // 
+            // decPoint
+            // 
+            decPoint.Location = new Point(157, 407);
+            decPoint.Margin = new Padding(3, 4, 3, 4);
+            decPoint.Name = "decPoint";
+            decPoint.Size = new Size(65, 53);
+            decPoint.TabIndex = 1;
+            decPoint.Text = ".";
+            decPoint.UseVisualStyleBackColor = true;
+            // 
+            // addOp
+            // 
+            addOp.Location = new Point(229, 407);
+            addOp.Margin = new Padding(3, 4, 3, 4);
+            addOp.Name = "addOp";
+            addOp.Size = new Size(73, 53);
+            addOp.TabIndex = 2;
+            addOp.Text = "+";
+            addOp.UseVisualStyleBackColor = true;
+            // 
+            // oneKey
+            // 
+            oneKey.Location = new Point(5, 345);
+            oneKey.Margin = new Padding(3, 4, 3, 4);
+            oneKey.Name = "oneKey";
+            oneKey.Size = new Size(71, 53);
+            oneKey.TabIndex = 3;
+            oneKey.Text = "1";
+            oneKey.UseVisualStyleBackColor = true;
+            // 
+            // twoKey
+            // 
+            twoKey.Location = new Point(82, 345);
+            twoKey.Margin = new Padding(3, 4, 3, 4);
+            twoKey.Name = "twoKey";
+            twoKey.Size = new Size(67, 53);
+            twoKey.TabIndex = 4;
+            twoKey.Text = "2";
+            twoKey.UseVisualStyleBackColor = true;
+            // 
+            // threeKey
+            // 
+            threeKey.Location = new Point(157, 345);
+            threeKey.Margin = new Padding(3, 4, 3, 4);
+            threeKey.Name = "threeKey";
+            threeKey.Size = new Size(65, 53);
+            threeKey.TabIndex = 5;
+            threeKey.Text = "3";
+            threeKey.UseVisualStyleBackColor = true;
+            // 
+            // subtractOp
+            // 
+            subtractOp.Location = new Point(229, 345);
+            subtractOp.Margin = new Padding(3, 4, 3, 4);
+            subtractOp.Name = "subtractOp";
+            subtractOp.Size = new Size(73, 53);
+            subtractOp.TabIndex = 6;
+            subtractOp.Text = "-";
+            subtractOp.UseVisualStyleBackColor = true;
+            // 
+            // fourKey
+            // 
+            fourKey.Location = new Point(5, 284);
+            fourKey.Margin = new Padding(3, 4, 3, 4);
+            fourKey.Name = "fourKey";
+            fourKey.Size = new Size(71, 53);
+            fourKey.TabIndex = 7;
+            fourKey.Text = "4";
+            fourKey.UseVisualStyleBackColor = true;
+            // 
+            // fiveKey
+            // 
+            fiveKey.Location = new Point(82, 284);
+            fiveKey.Margin = new Padding(3, 4, 3, 4);
+            fiveKey.Name = "fiveKey";
+            fiveKey.Size = new Size(67, 53);
+            fiveKey.TabIndex = 8;
+            fiveKey.Text = "5";
+            fiveKey.UseVisualStyleBackColor = true;
+            // 
+            // equalOp
+            // 
+            equalOp.Location = new Point(309, 352);
+            equalOp.Margin = new Padding(3, 4, 3, 4);
+            equalOp.Name = "equalOp";
+            equalOp.Size = new Size(69, 108);
+            equalOp.TabIndex = 9;
+            equalOp.Text = "=";
+            equalOp.UseVisualStyleBackColor = true;
+            // 
+            // inverseX
+            // 
+            inverseX.Location = new Point(309, 247);
+            inverseX.Margin = new Padding(3, 4, 3, 4);
+            inverseX.Name = "inverseX";
+            inverseX.Size = new Size(69, 97);
+            inverseX.TabIndex = 10;
+            inverseX.Text = "1/x";
+            inverseX.UseVisualStyleBackColor = true;
+            // 
+            // squareRoot
+            // 
+            squareRoot.Location = new Point(309, 163);
+            squareRoot.Margin = new Padding(3, 4, 3, 4);
+            squareRoot.Name = "squareRoot";
+            squareRoot.Size = new Size(69, 76);
+            squareRoot.TabIndex = 11;
+            squareRoot.Text = "Sqrt";
+            squareRoot.UseVisualStyleBackColor = true;
+            // 
+            // sixKey
+            // 
+            sixKey.Location = new Point(157, 284);
+            sixKey.Margin = new Padding(3, 4, 3, 4);
+            sixKey.Name = "sixKey";
+            sixKey.Size = new Size(65, 53);
+            sixKey.TabIndex = 12;
+            sixKey.Text = "6";
+            sixKey.UseVisualStyleBackColor = true;
+            sixKey.Click += sixKey_Click;
+            // 
+            // multOp
+            // 
+            multOp.Location = new Point(229, 284);
+            multOp.Margin = new Padding(3, 4, 3, 4);
+            multOp.Name = "multOp";
+            multOp.Size = new Size(73, 53);
+            multOp.TabIndex = 13;
+            multOp.Text = "*";
+            multOp.UseVisualStyleBackColor = true;
+            // 
+            // sevenKey
+            // 
+            sevenKey.Location = new Point(5, 223);
+            sevenKey.Margin = new Padding(3, 4, 3, 4);
+            sevenKey.Name = "sevenKey";
+            sevenKey.Size = new Size(71, 53);
+            sevenKey.TabIndex = 14;
+            sevenKey.Text = "7";
+            sevenKey.UseVisualStyleBackColor = true;
+            // 
+            // eightKey
+            // 
+            eightKey.Location = new Point(82, 223);
+            eightKey.Margin = new Padding(3, 4, 3, 4);
+            eightKey.Name = "eightKey";
+            eightKey.Size = new Size(67, 53);
+            eightKey.TabIndex = 15;
+            eightKey.Text = "8";
+            eightKey.UseVisualStyleBackColor = true;
+            // 
+            // nineKey
+            // 
+            nineKey.Location = new Point(157, 223);
+            nineKey.Margin = new Padding(3, 4, 3, 4);
+            nineKey.Name = "nineKey";
+            nineKey.Size = new Size(65, 53);
+            nineKey.TabIndex = 16;
+            nineKey.Text = "9";
+            nineKey.UseVisualStyleBackColor = true;
+            // 
+            // divideOp
+            // 
+            divideOp.Location = new Point(229, 223);
+            divideOp.Margin = new Padding(3, 4, 3, 4);
+            divideOp.Name = "divideOp";
+            divideOp.Size = new Size(73, 53);
+            divideOp.TabIndex = 17;
+            divideOp.Text = "/";
+            divideOp.UseVisualStyleBackColor = true;
+            // 
+            // backKey
+            // 
+            backKey.Location = new Point(5, 163);
+            backKey.Margin = new Padding(3, 4, 3, 4);
+            backKey.Name = "backKey";
+            backKey.Size = new Size(71, 52);
+            backKey.TabIndex = 18;
+            backKey.Text = "<---";
+            backKey.UseVisualStyleBackColor = true;
+            // 
+            // clearAllKey
+            // 
+            clearAllKey.Location = new Point(82, 163);
+            clearAllKey.Margin = new Padding(3, 4, 3, 4);
+            clearAllKey.Name = "clearAllKey";
+            clearAllKey.Size = new Size(67, 52);
+            clearAllKey.TabIndex = 19;
+            clearAllKey.Text = "CE";
+            clearAllKey.UseVisualStyleBackColor = true;
+            // 
+            // posNeg
+            // 
+            posNeg.Location = new Point(229, 163);
+            posNeg.Margin = new Padding(3, 4, 3, 4);
+            posNeg.Name = "posNeg";
+            posNeg.Size = new Size(73, 52);
+            posNeg.TabIndex = 20;
+            posNeg.Text = "+/-";
+            posNeg.UseVisualStyleBackColor = true;
+            // 
+            // clearLastKey
+            // 
+            clearLastKey.Location = new Point(157, 163);
+            clearLastKey.Margin = new Padding(3, 4, 3, 4);
+            clearLastKey.Name = "clearLastKey";
+            clearLastKey.Size = new Size(65, 52);
+            clearLastKey.TabIndex = 21;
+            clearLastKey.Text = "C";
+            clearLastKey.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(8, 15);
+            richTextBox1.Margin = new Padding(3, 4, 3, 4);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(370, 127);
+            richTextBox1.TabIndex = 22;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(382, 476);
+            Controls.Add(richTextBox1);
+            Controls.Add(clearLastKey);
+            Controls.Add(posNeg);
+            Controls.Add(clearAllKey);
+            Controls.Add(backKey);
+            Controls.Add(divideOp);
+            Controls.Add(nineKey);
+            Controls.Add(eightKey);
+            Controls.Add(sevenKey);
+            Controls.Add(multOp);
+            Controls.Add(sixKey);
+            Controls.Add(squareRoot);
+            Controls.Add(inverseX);
+            Controls.Add(equalOp);
+            Controls.Add(fiveKey);
+            Controls.Add(fourKey);
+            Controls.Add(subtractOp);
+            Controls.Add(threeKey);
+            Controls.Add(twoKey);
+            Controls.Add(oneKey);
+            Controls.Add(addOp);
+            Controls.Add(decPoint);
+            Controls.Add(zeroKey);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button zeroKey;
+        private Button decPoint;
+        private Button addOp;
+        private Button oneKey;
+        private Button twoKey;
+        private Button threeKey;
+        private Button subtractOp;
+        private Button fourKey;
+        private Button fiveKey;
+        private Button equalOp;
+        private Button inverseX;
+        private Button squareRoot;
+        private Button sixKey;
+        private Button multOp;
+        private Button sevenKey;
+        private Button eightKey;
+        private Button nineKey;
+        private Button divideOp;
+        private Button backKey;
+        private Button clearAllKey;
+        private Button posNeg;
+        private Button clearLastKey;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ M11.Calculator/Form1.cs
@@ -0,0 +1,21 @@
+namespace M11.Calculator
+{
+    public partial class Form1 : Form
+    {
+        AbsState state; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zeroKey_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sixKey_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
